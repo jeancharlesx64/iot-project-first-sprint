f6d3699499cc383bee8ee4ce2d1d40b6f6b695e0
IsabellaMatos44801 <isabella.messias@sptech.school>
2024-03-20 23:16:05 -0300

    version final database

--- prototyping/databases/IsabellaMatos44801/IndividualBD (1).sql
+++ prototyping/databases/IsabellaMatos44801/IndividualBD (1).sql
@@ -1,59 +1,80 @@
 USE sprint;
-CREATE TABLE cliente (
-idEmpresa int PRIMARY KEY AUTO_INCREMENT,
-nome varchar(50),
-cnpj varchar(20),
-email varchar(100),
-estado varchar(50),
-cidade varchar(100),
-bairro varchar(100),
-cep varchar(20),
-rua varchar(50),
-qntHec decimal(10,3));
 
-INSERT INTO cliente VALUES
-(DEFAULT, 'AGROSUL', '40512337000100', 'gerencia.servicos@agrosuljd.com.br', 'São Paulo', 'São Paulo', 'Vila Matilde', '11703210', 'Rua Juréia', '2.500'),
-(DEFAULT, 'AGROTOP', '40512337000122', 'agos@agrotop.com.br', 'São Paulo', 'São Paulo', 'Vila Mariana', '1170310', 'Rua Salvador', '1.500'),
-(DEFAULT, 'Agrobras', '12345678901234', 'contato@agrobras.com.br', 'São Paulo', 'São Paulo', 'Campo Limpo', '05871010', 'Av. Interlagos', '3000.000'),
-(DEFAULT, 'Agricultura RJ', '98765432109876', 'contato@agriculturarj.com.br', 'Rio de Janeiro', 'Rio de Janeiro', 'Copacabana', '22050010', 'Av. Atlântica', '1800.000'),
-(DEFAULT, 'Agrocampo', '45612378904567', 'contato@agrocampo.com.br', 'São Paulo', 'São Paulo', 'Moema', '04071020', 'Rua Normandia', '2200.000'),
-(DEFAULT, 'Agroverde', '78965412309876', 'contato@agroverde.com.br', 'Rio de Janeiro', 'Rio de Janeiro', 'Botafogo', '22250040', 'Rua Voluntários da Pátria', '1400.000');
+CREATE TABLE Empresa ( -- Empresas que já fecharam contrato.
+idEmpresa INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+nome VARCHAR (50) NOT NULL,
+cnpj VARCHAR (20) NOT NULL,
+email VARCHAR (100) NOT NULL,
+estado VARCHAR (50) NOT NULL,
+cidade VARCHAR (100) NOT NULL,
+bairro VARCHAR (100) NOT NULL,
+cep VARCHAR (20) NOT NULL,
+rua VARCHAR (50) NOT NULL);
 
-CREATE TABLE sensor (
-idSensor INT PRIMARY KEY AUTO_INCREMENT,
-codigo varchar(70),
-tipo varchar(5),
-dtAluguel date,
-empresa varchar(50));
+INSERT INTO Empresa (nome, cnpj, email, estado, cidade, bairro, cep, rua) VALUES
+('AGROSUL', '40512337000100', 'gerencia.servicos@agrosuljd.com.br', 'São Paulo', 'São Paulo', 'Vila Matilde', '11703210', 'Rua Juréia'),
+('AGROTOP', '40512337000122', 'agos@agrotop.com.br', 'São Paulo', 'São Paulo', 'Vila Mariana', '1170310', 'Rua Salvador'),
+('Agrobras', '12345678901234', 'contato@agrobras.com.br', 'São Paulo', 'São Paulo', 'Campo Limpo', '05871010', 'Av. Interlagos'),
+('Agricultura RJ', '98765432109876', 'contato@agriculturarj.com.br', 'Rio de Janeiro', 'Rio de Janeiro', 'Copacabana', '22050010', 'Av. Atlântica'),
+('Agrocampo', '45612378904567', 'contato@agrocampo.com.br', 'São Paulo', 'São Paulo', 'Moema', '04071020', 'Rua Normandia'),
+('Agroverde', '78965412309876', 'contato@agroverde.com.br', 'Rio de Janeiro', 'Rio de Janeiro', 'Botafogo', '22250040', 'Rua Voluntários da Pátria');
 
-ALTER TABLE sensor ADD COLUMN temperaturaLM35 DECIMAL (5,0);
-ALTER TABLE sensor ADD COLUMN temperaturDHT11 DECIMAL (5,0);
+INSERT INTO Empresa (nome, cnpj, email, estado, cidade, bairro, cep, rua) VALUES
+('Nova Empresa', '12345678901234', 'contato@novaempresa.com.br', 'São Paulo', 'São Paulo', 'Novo Bairro', '12345678', 'Rua Nova');
+
+CREATE TABLE Sensor ( -- Informações sobre o sensor.
+idSensor INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+codigo VARCHAR (6) NOT NULL, -- Identificação do sensor.
+tipo VARCHAR (5) NOT NULL, -- Temperatura ou umidade.
+dtAluguel DATE NOT NULL, -- Data em que foi alugado por tal empresa.
+empresa VARCHAR (50) NOT NULL); -- Nome da empresa que alugou.
+
+INSERT INTO Sensor (codigo, tipo, dtAluguel, empresa) VALUES
+('ABC123', 'LM35', '2024-03-14', 'AGROSUL'),
+('DEF456', 'DHT11', '2024-03-14', 'AGROSUL'),
+('GHI789', 'LM35', '2024-03-20','AGROTOP'),
+('JKL012', 'DHT11', '2024-03-20', 'AGROTOP'),
+('MNO345', 'LM35', '2024-03-25', 'Agrobras'),
+('PQR678', 'DHT11', '2024-03-25', 'Agrobras'),
+('STU901', 'LM35', '2024-03-28', 'Agricultura'),
+('VWX234', 'DHT11', '2024-03-28', 'Agricultura'),
+('YZA567', 'LM35', '2024-04-01', 'Agrocampo'),
+('MNO345', 'DHT11', '2024-04-01', 'Agrocampo');
+
+INSERT INTO Sensor (codigo, tipo, dtAluguel, empresa) VALUES
+('XYZ123', 'LM35', '2024-04-10', 'NovaEmpresa');
+
+ALTER TABLE Sensor ADD CONSTRAINT chksensor CHECK(tipo IN('LM35','DHT11'));
+
+CREATE TABLE Armazém ( -- Local de armazenamento de milho de tal empresa.
+idArmazem INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+empresa  VARCHAR (50) NOT NULL, 
+capacidade VARCHAR (50) NOT NULL, -- Capacidade máxima em KG de armazenamento de milho.
+cep VARCHAR (20) NOT NULL);
+
+INSERT INTO Armazém (empresa, capacidade, cep) VALUES 
+('AGROSUL','5.000','11703210'),
+('AGROTOP','6.000','1170310');
+
+SELECT * FROM Sensor;
+SELECT * FROM Empresa;
+SELECT * FROM Sensor WHERE empresa = 'AGROSUL';
+SELECT * FROM Empresa WHERE estado = 'São Paulo' ORDER BY nome;
+SELECT * FROM Empresa WHERE estado = 'São Paulo' ORDER BY email DESC;
+SELECT * FROM Empresa WHERE nome LIKE 'Agro%';
+SELECT * FROM Sensor WHERE codigo LIKE '%456%';
+SELECT * FROM Sensor WHERE dtAluguel LIKE '2024-03%';
+SHOW TABLES;
+DESC Empresa;
+DESC Sensor;
+DESC Armazém;
 
 
-INSERT INTO sensor VALUES
-(DEFAULT,'ABC123', 'LM35', '2024-03-14', 'AGROSUL'),
-(DEFAULT,'DEF456', 'DHT11', '2024-03-14', 'AGROSUL'),
-(DEFAULT,'GHI789', 'LM35', '2024-03-20','AGROTOP'),
-(DEFAULT,'JKL012', 'DHT11', '2024-03-20', 'AGROTOP'),
-(DEFAULT,'MNO345', 'LM35', '2024-03-25', 'Agrobras'),
-(DEFAULT,'PQR678', 'DHT11', '2024-03-25', 'Agrobras'),
-(DEFAULT,'STU901', 'LM35', '2024-03-28', 'Agricultura'),
-(DEFAULT,'VWX234', 'DHT11', '2024-03-28', 'Agricultura'),
-(DEFAULT,'YZA567', 'LM35', '2024-04-01', 'Agrocampo'),
-(DEFAULT,'MNO345', 'DHT11', '2024-04-01', 'Agrocampo');
+UPDATE Empresa SET estado ="São Paulo" WHERE idEmpresa = 4;
+UPDATE Sensor SET tipo = "LM35" WHERE idSensor = 4;
 
-ALTER TABLE sensor
-ADD CONSTRAINT chksensor CHECK(tipo IN('LM35','DHT11'));
-
-SELECT * FROM sensor;
-SELECT * FROM cliente;
-SELECT * FROM sensor WHERE empresa = 'AGROSUL';
-SELECT * FROM cliente WHERE estado = 'São Paulo' ORDER BY nome;
-SELECT * FROM cliente WHERE qntHec > 2000;
-SELECT * FROM cliente WHERE nome LIKE 'Agro%';
-SELECT * FROM sensor WHERE codigo LIKE '%456%';
-SELECT * FROM sensor WHERE dtAluguel LIKE '2024-03%';
-
-INSERT INTO cliente VALUES 
-(DEFAULT,'Nova Empresa', '12345678901234', 'contato@novaempresa.com.br', 'São Paulo', 'São Paulo', 'Novo Bairro', '12345678', 'Rua Nova', 1500.000);
-INSERT INTO sensor VALUES (DEFAULT,'XYZ123', 'LM35', '2024-04-10', 'NovaEmpresa');
+DELETE FROM Empresa WHERE idEmpresa = 7;
+DELETE FROM Sensor WHERE idSensor = 4;
+DROP TABLE Empresa;
+DROP TABLE Sensor;
+DROP TABLE Armazém;
